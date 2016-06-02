5eb2a30ec0f995a72241bf695bfae29cc1be04ff
Julius Dewavrin <jdewavrin@gmail.com>
2016-06-02 11:36:41 +0200

    model spec validated

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,3 +1,6 @@
 class Cocktail < ActiveRecord::Base
-  has_many :doses
+  has_many :doses, dependent: :destroy
+  has_many :ingredients, through: :doses
+  # Through set le has many ingredient via la table doses
+  validates :name, presence: true, uniqueness: true
 end
